433f6e5a634307460faa2749030b880de293809e
Pedro Rico Pinazo <ricopinazo@gmail.com>
2025-04-06 21:33:11 +0100

    add some logs

--- src/nixpacks/mod.rs
+++ src/nixpacks/mod.rs
@@ -131,6 +131,9 @@ mod provider_tests {
 
         let command = format!("cp -r resources/astro-ssr/ {}", path.to_str().unwrap());
         exec(&PathBuf::from("."), &command).await;
+        exec(path, "echo --------------").await;
+        exec(path, "pwd").await;
+        exec(path, "ls").await;
 
         create_docker_image_with_nixpacks(path, vec!["HOST=0.0.0.0", "PORT=80"])
             .await
